9c1fcaf96d1de47d9bed33eada5159b6b59a9f84
Adam Hani Schakaki <adam@schakaki.net>
2013-04-30 14:33:30 +0100

    Added Icon

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     app=['main.py'],
+    date_files=['--iconfile'],
+    options={'py2app': {'iconfile': 'icon.icns'}},
     setup_requires=['py2app'],
     name='ShnergleDevClient'
 )
