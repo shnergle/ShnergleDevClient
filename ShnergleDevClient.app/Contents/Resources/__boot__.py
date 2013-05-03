d168839c35b6a18309ef1806c4a2552ff4a2e205
Adam Hani Schakaki <adam@schakaki.net>
2013-05-03 21:12:20 +0100

    Update

--- ShnergleDevClient.app/Contents/Resources/__boot__.py
+++ ShnergleDevClient.app/Contents/Resources/__boot__.py
@@ -1,12 +1,3 @@
-def _reset_sys_path():
-    # Clear generic sys.path[0]
-    import sys, os
-    resources = os.environ['RESOURCEPATH']
-    while sys.path[0] == resources:
-        del sys.path[0]
-_reset_sys_path()
-
-
 def _chdir_resource():
     import os
     os.chdir(os.environ['RESOURCEPATH'])
@@ -22,22 +13,19 @@ def _disable_linecache():
 _disable_linecache()
 
 
-def _run():
+def _run(*scripts):
     global __file__
     import os, sys, site
     sys.frozen = 'macosx_app'
     base = os.environ['RESOURCEPATH']
+    site.addsitedir(base)
+    site.addsitedir(os.path.join(base, 'Python', 'site-packages'))
+    if not scripts:
+        import __main__
+    for script in scripts:
+        path = os.path.join(base, script)
+        sys.argv[0] = __file__ = path
+        execfile(path, globals(), globals())
 
-    argv0 = os.path.basename(os.environ['ARGVZERO'])
-    script = SCRIPT_MAP.get(argv0, DEFAULT_SCRIPT)
 
-    path = os.path.join(base, script)
-    sys.argv[0] = __file__ = path
-    with open(path, 'rU') as fp:
-        source = fp.read() + "\n"
-    exec(compile(source, path, 'exec'), globals(), globals())
-
-
-DEFAULT_SCRIPT='main.py'
-SCRIPT_MAP={}
-_run()
+_run('main.py')
